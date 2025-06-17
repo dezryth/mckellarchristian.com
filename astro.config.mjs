1e35d6c33788029afd87ff905a7f87b876cefe82
Scott Christian <scott@codesilver.io>
2025-06-17 16:41:28 -0500

    Work dammit!

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mckellarchristian.com',
+  site: 'https://mckellarchristian.com',
   base: '/',
   outDir: './docs', // Change the build output folder to 'docs'
 	integrations: [mdx(), sitemap()],
